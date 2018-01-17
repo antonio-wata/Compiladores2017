8739b7b4cbeaeea3be57f07a149b8b2d65bf5e06
Diego <dmagaram666@gmail.com>
2018-01-17 16:59:31 -0600

    Cambios y comentarios

--- intermediate_code.h
+++ intermediate_code.h
@@ -3,37 +3,41 @@
 
 #include <string.h>
 
-
+/* Estructura de una cuadrupla. */
+// op - Operacion que se va a realizar.
+// arg1 - Argumento 1.
+// arg2 - Argumento 2.
+// res - Donde se va a almacenar el resultado.
 typedef struct _quad{
-	char* op; //op[20];
-	char* arg1; //arg1[50];
-	char* arg2; //arg2[20];
-	char* res; //res[20];
+	char* op;
+	char* arg1;
+	char* arg2;
+	char* res;
 } quad;
 
+/* Estructura de la tabla de cuadruplas. */
+// items - Lista de cuadruplas almacenadas.
+// i - Total de cuadruplas guardadas.
 typedef struct _intermediate_code{
-	quad* items; //items[10000];
+	quad* items;
 	int i;
 } intermediate_code;
 
 typedef struct _label{
-	int* items; //items[100];
+	int* items;
 	int i;
 } label;
 
+/* Tabla que usara el compilador para guardar las cuadruplas. */
 intermediate_code CODE;
 
+// Funciones:
+
 void init_code();
-
 int gen_code(char *op , char *arg1, char *arg2, char *res);
-
-
 label create_list(int l);
-
 label merge(label l1, label l2);
-
 void backpatch(label l, int inst);
-
 void print_code();
 
 #endif
